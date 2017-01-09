9933e3716f9a9a7cd0e34583c9318a108048d105
Ali Zaidi <alikaz.zaidi@gmail.com>
2017-01-08 20:34:53 -0500

    update shiny download links'

--- inst/install-shiny-centos.sh
+++ inst/install-shiny-centos.sh
@@ -2,13 +2,12 @@
 
 ## install rstudio
 
-SHINY_VERSION=1.0.136
+SHINY_VERSION=1.5.1.834
 
 # SHINY_FILE=shiny-server-"$SHINY_VERSION"-amd64.deb
 SHINY_FILE=shiny-server-"$SHINY_VERSION"-rh5-x86_64.rpm
 
-
-wget https://s3.amazonaws.com/rstudio-dailybuilds/"$SHINY_FILE"
+wget https://s3.amazonaws.com/rstudio-shiny-server-os-build/centos5.9/x86_64/"$SHINY_FILE"
 
 # sudo gdebi "$SHINY_FILE"
 sudo yum install --nogpgcheck "$SHINY_FILE"
--- inst/install-shiny-ubuntu.sh
+++ inst/install-shiny-ubuntu.sh
@@ -2,11 +2,11 @@
 
 ## install rstudio
 
-SHINY_VERSION=1.0.136
+SHINY_VERSION=1.5.1.834
 
 SHINY_FILE=shiny-server-"$SHINY_VERSION"-amd64.deb
 
-wget https://s3.amazonaws.com/rstudio-dailybuilds/"$SHINY_FILE"
+wget https://s3.amazonaws.com/rstudio-shiny-server-os-build/ubuntu-12.04/x86_64/"$SHINY_FILE"
 
 sudo gdebi "$SHINY_FILE"
 
