bef0629cb73e24291f30e24b89734c8f1e6d5a21
Ali Zaidi <alikaz.zaidi@gmail.com>
2019-05-01 18:05:03 -0700

    FIX: no activate

--- az-cli-scripts/tag-resources.sh
+++ az-cli-scripts/tag-resources.sh
@@ -1,4 +1,3 @@
-source activate py35
 current_month=$(date +%B)
 az group show -n $1 --query tags
 az group update -n $1 --set tags.Owner=alizaidi tags.CreationDate=$2 tags.Description=$3 tags.KeepAlive=$current_month
