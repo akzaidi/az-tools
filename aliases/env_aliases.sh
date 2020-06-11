227654d2110848578cabd7f0e67122387875ec98
Ali Zaidi <alikaz.zaidi@gmail.com>
2020-06-11 10:30:19 -0700

    ADD: docker aliases

--- aliases/env_aliases.sh
+++ aliases/env_aliases.sh
@@ -4,3 +4,16 @@
 alias cie='conda info --envs'
 function ca() { conda activate "$@"  }
 
+## docker
+# list all images
+alias dri='docker images'
+# list all containers
+alias drc='docker ps -a'
+# delete container
+drm() { docker rm $1}
+# delete image
+drmi() { docker rmi $1}
+# Bash into running container
+dbash() { docker exec -it $(docker ps -aqf "name=$1") bash; }
+# Dockerfile build, e.g., $dbu alizaidi/test
+dbu() { docker build -t=$1 .; }
